159516b5a07e73edadbcf567fe70b86fadd2dec2
Elif Kaner <elifkanerx@gmail.com>
2026-07-13 13:55:46 +0300

    Yeni ürün ekleme ekranında boş değer girildiğinde gösterilecek zorunlu alan hata mesajları eklendi

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://192.168.2.176:5000/api/:path*',
+      },
+    ];
+  },
   /* config options here */
 };
 
