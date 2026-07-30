7aac5a1659a2e12a904f360af24bf4bc33928d2d
Elif Kaner <elifkanerx@gmail.com>
2026-07-30 09:41:43 +0300

    Projeye AuuthGuard eklenerek geç tetiklenen UseEffect sonucu login olmadan renderlama sorunu çözüldü.

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import SideNav from '@/app/ui/sidenav';
+import AuthGuard from '@/app/ui/auth-guard';
 
 export default function DashboardLayout({
   children,
@@ -7,13 +8,15 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-50">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <AuthGuard>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-50">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-grow md:overflow-y-auto">
+          {children}
+        </div>
       </div>
-      <div className="flex-grow md:overflow-y-auto">
-        {children}
-      </div>
-    </div>
+    </AuthGuard>
   );
 }
